adb02f31c8f0371675f7cfccbef4ac18dbc82786
Jacob <jakeboone@aol.com>
2014-07-14 18:02:13 -0600

    cleaned up comments

--- main.js
+++ main.js
@@ -2,66 +2,36 @@
 // want Hover function to have a "glow around the icon"
 // I want the description to go to a div that toggles open upon click of the image. and goes away if another icon is clicked
 
+// constructor
+var SolarItem = function(name, description, image){
+	this.name = name;
+	this.description = description;
+	this.image = image;
 
-// namesake
-// var SolarSite = (function(){
-		// constructor
-		var SolarItem = function(name, description, image){
-			this.name = name;
-			this.description = description;
-			this.image = image;
-
-		}
-
-		// click handler to display the decription later below the jumbotron div
-
-		// SolarItem.prototype.clickHandler = function(){
-		// 		// var self = this;
-		// 		this.on('click', function(){
-		// 		$('.description').append(this.decription)
-		// 		// }
-				
-		// }
+}
 
 
+// Declaring the images as objects
+var imgPanel = $('<img id="solarpanel" class="solar-icon col-md-2">').attr('src', 'images/panel.png');
+var imgController = $('<img id="chargecontroller" class="solar-icon col-md-1">').attr('src', 'images/controller.png');
+var imgBattery = $('<img id="battery" class="solar-icon col-md-2">').attr('src', 'images/batt2.png');
+var imgInverter = $('<img id="inverter" class="solar-icon col-md-1">').attr('src', 'images/inverter.png');
+var imgHome = $('<img id="house" class="solar-icon col-md-2">').attr('src', 'images/home.png');
 
-		// Declaring the images as objects
-		var imgPanel = $('<img id="solarpanel" class="solar-icon col-md-2">').attr('src', 'images/panel.png');
-		var imgController = $('<img id="chargecontroller" class="solar-icon col-md-1">').attr('src', 'images/controller.png');
-		var imgBattery = $('<img id="battery" class="solar-icon col-md-2">').attr('src', 'images/batt2.png');
-		var imgInverter = $('<img id="inverter" class="solar-icon col-md-1">').attr('src', 'images/inverter.png');
-		var imgHome = $('<img id="house" class="solar-icon col-md-2">').attr('src', 'images/home.png');
-
-		// Bank Items with 'description' and the varible for the the image objects
-		var panel = new SolarItem('solarpanel', 'Panel description', imgPanel);
-		var controller = new SolarItem('chargecontroller','Charge controller description', imgController);
-		var battery = new SolarItem('battery','Battery description', imgBattery);
-		var inverter = new SolarItem('inverter','Inverter description', imgInverter);
-		var home = new SolarItem('house','Power On', imgHome);
-
-
-
-
+// Bank Items with 'description' and the varible for the the image objects
+var panel = new SolarItem('solarpanel', 'Panel description', imgPanel);
+var controller = new SolarItem('chargecontroller','Charge controller description', imgController);
+var battery = new SolarItem('battery','Battery description', imgBattery);
+var inverter = new SolarItem('inverter','Inverter description', imgInverter);
+var home = new SolarItem('house','Power On', imgHome);
 
 		
-		// Declare array of the system.
-		var system = [panel, controller, battery, inverter, home];
+// Declare array of the system.
+var system = [panel, controller, battery, inverter, home];
 
 
-		// var information = null;
-
-		// var init = function(){
-		// 	// create the system
-		// 	var solarEl = ?.create();
-		// 	$('#full-system').append(this);
-
-		// }
-
-// })();
 
 $(document).on('ready', function() {
- 
-	
 		// // loop thru the array of system to display the image on the page
 		var putimagesonpage = function(arr){
 			// var arrSystem = arr.image.splice()['~~>']
@@ -89,28 +59,8 @@ $(document).on('ready', function() {
 			var output = _.findWhere(system,{name: idVal})
 				console.log(output)
 			$('.description').append(output.description);
-
-				
-			
-		 
-
 			console.log("clicked")
 		});
-
-	
-
-		
-// render the page with image and description
-  // SolarSite.init();
-
-
-	// $('#full-system').append(panel.image);
-	// $('#full-system').append(controller.image);
-	// $('#full-system').append(battery.image);
-	// $('#full-system').append(inverter.image);
-	// $('#full-system').append(home.image);
-
-
 });
 
 
